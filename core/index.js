f4239b8c00357d761fc5a89f88884cc26c6fdbb2
Tierney Cyren <hello@bnb.im>
2022-02-25 17:31:58 -0500

    temp: some improvements
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- core/index.js
+++ core/index.js
@@ -1,5 +1,5 @@
 const { fetch } = require('undici')
-const { DateTime } = require('luxon')
+const { DateTime, Interval } = require('luxon')
 const semver = require('semver')
 
 async function inver() {
@@ -50,18 +50,33 @@ async function inver() {
     if (!data[name].support) { // check to see if we've already written it. if we have, we don't need to waste time on it.
       data[name].support = {}
       data[name].support.codename = schedule[name]?.codename ?? undefined
+      data[name].support.lts = schedule[name]?.lts ? {} : undefined
+
+      // run this the first time we start working on the support object,
+      // since that will be the newest version
+      if(versions[version].lts) {
+        data[name].support.lts.newest = versionSemver.version
+      }
 
       data[name].support.phases = {}
       data[name].support.phases.start = schedule[name]?.start ?? undefined
       data[name].support.phases.lts = schedule[name]?.lts ?? undefined
       data[name].support.phases.maintenance = schedule[name]?.maintenance ?? undefined
       data[name].support.phases.end = schedule[name]?.end ?? undefined
-      await determineCurrentReleasePhase(data[name].support.phases)
-  
-      // WIP:
-      // - newest
-      // - oldest
-      // - phase
+      data[name].support.phases.current = ''
+    }
+
+    if(schedule[name]?.start !== undefined) { // hack-y way to skip this logic on releases that don't have a listed start
+      console.log(versions[version].version, await determineCurrentReleasePhase(now, data[name].support.phases))
+    }
+
+    // this is a slightly inefficient way to do this but it's also easy
+    //
+    // tl;dr we're just assigning this every single iteration and the last
+    // iteration will be the oldest version, since we're going from newest
+    // to oldest
+    if(versions[version].lts) {
+      data[name].support.lts.oldest = versionSemver.version
     }
 
     // ## define the lts object in each specific version
@@ -70,7 +85,7 @@ async function inver() {
     data[name].releases[`v${versionSemver.version}`].lts.isLts = versions[version].lts ? true : false
 
     // # Security
-    // define the release-line specific security object
+    // ## define the release-line specific security object
     if (!data[name].security) { // check to see if we've already written it. if we have, we don't need to waste time on it.
       data[name].security = {}
       if(!data[name].security.newest) {
@@ -88,6 +103,7 @@ async function inver() {
       // add every other sevurity release to the 'all' array after the first time we encoutner one
       data[name].security.all.push(versionSemver.version)
     }
+
     // ## define the security object in each specfic version
     data[name].releases[`v${versionSemver.version}`].security = {}
 
@@ -98,22 +114,46 @@ async function inver() {
   console.log(JSON.stringify(data, null, 2))
 }
 
-async function determineCurrentReleasePhase(dates = {}) {
+async function determineCurrentReleasePhase(now, dates = {}) {
+  // here we figure out if the dates for each release line passed is in the past or future
+  // `true` is in the past
+  // `false` is in the future
   const isoified = {
-    start: await nanCheck(DateTime.fromISO(dates.start)) ?? undefined,
-    lts: await nanCheck(DateTime.fromISO(dates.lts)) ?? undefined,
-    maintenance: await nanCheck(DateTime.fromISO(dates.maintenance)) ?? undefined,
-    end: await nanCheck(DateTime.fromISO(dates.end)) ?? undefined
+    start: await isInPast(DateTime.fromISO(dates.start).diffNow().toMillis()) ?? undefined,
+    lts: await isInPast(DateTime.fromISO(dates.lts).diffNow().toMillis()) ?? undefined,
+    maintenance: await isInPast(DateTime.fromISO(dates.maintenance).diffNow().toMillis()) ?? undefined,
+    end: await isInPast(DateTime.fromISO(dates.end).diffNow().toMillis()) ?? undefined,
   }
 
-  return isoified
+  // set up our result to return
+  let result = undefined
+  
+  // iterate over the past/future object and set the above variable to whatever the first date is in the future.
+  Object.keys(isoified).forEach(phase => {
+    // since we're looping, the last true is the current phase
+    // since the start date will always be in the past
+    if(isoified[phase] === true) {
+        result = phase
+    }
+  })
+  return result
 }
 
-async function nanCheck(DateTimeInstance) {
-  if(!isNaN(DateTimeInstance)){
-    return DateTimeInstance
+// super hacky tool to convert our weird DateTime tooling into a boolean
+// specifically consumes the following:
+//
+// DateTime.fromISO(DATE).diffNow().toMillis()
+//
+// where DATE is your date value
+async function isInPast(number) {
+  const sign = Math.sign(number)
+  if(sign === -1 || sign === 0) {
+    return true
+  } else if(sign === 1) {
+    return false
   } else {
     return undefined
   }
 }
+
 inver()
