0b44cdff86cb0cb4cd764134cc44565064955d64
Tierney Cyren <hello@bnb.im>
2022-02-28 08:01:08 -0800

    chore: add releases data support
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- static/index.js
+++ static/index.js
@@ -1,7 +1,9 @@
 const default = require('./data/default.json')
+const releases = require('./data/releases.json')
 const security = require('./data/security.json')
 const support = require('./data/support.json')
 
 module.exports.default = default
+module.exports.releases = releases
 module.exports.security = security
 module.exports.support = support
