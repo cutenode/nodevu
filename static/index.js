4aee41a1e431d00de0fe76d93a96a01541024ff3
Tierney Cyren <hello@bnb.im>
2022-03-01 13:25:57 -0800

    feat: add static

--- static/index.js
+++ static/index.js
@@ -1,9 +1,9 @@
-const default = require('./data/default.json')
-const releases = require('./data/releases.json')
-const security = require('./data/security.json')
-const support = require('./data/support.json')
+const defaultData = require('./data/default.json')
+const releasesData = require('./data/releases.json')
+const securityData = require('./data/security.json')
+const supportData = require('./data/support.json')
 
-module.exports.default = default
-module.exports.releases = releases
-module.exports.security = security
-module.exports.support = support
+module.exports.default = defaultData
+module.exports.releases = releasesData
+module.exports.security = securityData
+module.exports.support = supportData
