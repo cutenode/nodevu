33e3bd30b6abea3b1f0b6bfa191817a05a8cf8d6
Tierney Cyren <hello@bnb.im>
2022-09-10 01:44:30 -0400

    test: some WIP changes that I thought I committed
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- core/test/test.js
+++ core/test/test.js
@@ -1,21 +1,22 @@
 const assert = require('assert')
-const nock = require('nock')
 const nodevu = require('../index')
-
+const { MockAgent, setGlobalDispatcher, fetch } = require('undici')
 const index = require('./data/index.json')
 const schedule = require('./data/schedule.json')
 
-nock('http://nodejs.org')
-  .get('/dist/index.json')
-  .reply(200, index)
+// this mock agent stuff isn't actually working for... some unkown reason
+const mock = new MockAgent()
+setGlobalDispatcher(mock)
 
-nock('https://raw.githubusercontent.com')
-  .get('/nodejs/Release/master/schedule.json')
-  .reply(200, schedule)
+const nodejsMock = mock.get('http://nodejs.org')
+nodejsMock.intercept({ path: '/dist/index.json', method: 'GET' }).reply(200, index)
+
+const githubMock = mock.get('https://raw.githubusercontent.com')
+githubMock.intercept({ path: '/nodejs/Release/master/schedule.json', method: 'GET' }).reply(200, schedule)
 
 describe('attempt to fetch data', async () => {
   it('should have some correct values for Node.js dependencies', async () => {
-    const data = await nodevu()
+    const data = await nodevu({ fetch: fetch })
     assert.deepStrictEqual(data.v17.releases['v17.0.0'].dependencies.npm, '8.1.0')
     assert.deepStrictEqual(data.v17.releases['v17.0.0'].dependencies.v8, '9.5.172.21')
     assert.deepStrictEqual(data.v17.releases['v17.0.0'].dependencies.uv, '1.42.0')
@@ -24,7 +25,7 @@ describe('attempt to fetch data', async () => {
   })
 
   it('should have some correct static values for support in a release line', async () => {
-    const data = await nodevu()
+    const data = await nodevu({ fetch: fetch })
     assert.deepStrictEqual(data.v14.support.codename, 'Fermium')
     assert.deepStrictEqual(data.v14.support.lts.newest, '14.19.0')
     assert.deepStrictEqual(data.v14.support.lts.oldest, '14.15.0')
@@ -35,7 +36,7 @@ describe('attempt to fetch data', async () => {
   })
 
   it('should have some correct dynamic values for support in a release line', async () => {
-    const data = await nodevu()
+    const data = await nodevu({ fetch: fetch })
     assert.deepStrictEqual(data.v8.support.phases.current, 'end')
     assert.deepStrictEqual(data.v14.support.phases.current, 'maintenance')
     assert.deepStrictEqual(data.v16.support.phases.current, 'lts')
