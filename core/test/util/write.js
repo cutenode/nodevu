d3fe3136a2a0eea9b1f4047d84ddbcdc9ceba91e
Tierney Cyren <hello@bnb.im>
2022-09-17 17:17:36 -0700

    test: remove code we'll never hit
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- core/test/util/write.js
+++ core/test/util/write.js
@@ -5,10 +5,6 @@ const { resolve } = require('node:path')
 async function write (path, data) {
   const outputJSON = JSON.stringify(data, null, 2)
   let filepath = resolve(path)
-  // hack to let us run this script from the root of the repo as a npm script with workspaces commands
-  if (filepath.includes('static/static/')) {
-    filepath = filepath.replace('static/static/', 'static/')
-  }
 
   await writeFile(filepath, outputJSON)
 }
