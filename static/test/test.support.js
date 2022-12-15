8b423ccf5380576846bccefbae2efe3d0af0c48b
Tierney Cyren <hello@bnb.im>
2022-12-14 20:56:08 -0500

    test: continue fixing linting
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- static/test/test.support.js
+++ static/test/test.support.js
@@ -7,24 +7,24 @@ const { describe, it } = require('test')
 //
 // these tests are also generally copy/pasted between the test files - both in this file and in core. why?
 // specifically because we want to be sure the data is consistent *and* because we want to be sure that
-// the various ways of accessing the data produce consistent results. 
+// the various ways of accessing the data produce consistent results.
 
-function check(data) {
-    // support checks
-    assert.deepStrictEqual(data.v14.codename, 'Fermium')
-    assert.deepStrictEqual(data.v12.lts.newest, '12.22.12')
-    assert.deepStrictEqual(data.v9.phases.current, 'end')
-    assert.deepStrictEqual(data.v8.phases.current, 'end')
+function check (data) {
+  // support checks
+  assert.deepStrictEqual(data.v14.codename, 'Fermium')
+  assert.deepStrictEqual(data.v12.lts.newest, '12.22.12')
+  assert.deepStrictEqual(data.v9.phases.current, 'end')
+  assert.deepStrictEqual(data.v8.phases.current, 'end')
 }
 
-describe("test known data in support.json export, being called from the default require", async () => {
-    it('should return the correct data for all types', () => {
-        check(staticData.support)
-    })
+describe('test known data in support.json export, being called from the default require', async () => {
+  it('should return the correct data for all types', () => {
+    check(staticData.support)
+  })
 })
 
-describe("test known data in support.json export, being called from a named require", async () => {
-    it('should return the correct data for all types', () => {
-        check(support)
-    })
+describe('test known data in support.json export, being called from a named require', async () => {
+  it('should return the correct data for all types', () => {
+    check(support)
+  })
 })
