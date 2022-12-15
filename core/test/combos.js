74f5b1c0e9278c41381817c4e06f6fdcc9b0bab1
Tierney Cyren <hello@bnb.im>
2022-12-14 21:01:02 -0500

    test: improve core tests and add static tests (#21)
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- core/test/combos.js
+++ core/test/combos.js
@@ -4,6 +4,21 @@ const { describe, it, beforeEach } = require('test')
 
 const beforeEachTemplate = require('../util/dev/beforeEachTemplate')
 
+function check (data) {
+  // releases checks
+  assert.deepStrictEqual(data.v17.releases['v17.0.0'].dependencies.npm, '8.1.0')
+  assert.deepStrictEqual(data.v17.releases['v17.0.0'].dependencies.v8, '9.5.172.21')
+  // support checks
+  assert.deepStrictEqual(data.v14.support.codename, 'Fermium')
+  assert.deepStrictEqual(data.v14.support.lts.newest, '14.19.0')
+  assert.deepStrictEqual(data.v9.support.phases.current, 'end')
+  assert.deepStrictEqual(data.v8.support.phases.current, 'end')
+  // security checks
+  assert.deepStrictEqual(data.v12.security.all.length, 14)
+  assert.deepStrictEqual(data.v12.security.newest, '12.22.9')
+  assert.deepStrictEqual(data.v12.security.oldest, '12.8.1')
+}
+
 const staticNow = require('./data/static/now.json')
 const now = JSON.parse(staticNow)
 
@@ -15,9 +30,8 @@ describe('check to make sure that combining options works as expected', async ()
       index: 'https://bnb.im/dist/index.json'
     }
     const staticData = await nodevu({ now, urls })
-    assert.deepStrictEqual(staticData.v17.releases['v17.0.0'].dependencies.npm, '8.1.0')
-    assert.deepStrictEqual(staticData.v14.support.codename, 'Fermium')
-    assert.deepStrictEqual(staticData.v8.support.phases.current, 'end')
+
+    check(staticData)
   })
 
   it('should still return valid data when the schedule is a different URL from the default while also using static now', async () => {
@@ -25,9 +39,8 @@ describe('check to make sure that combining options works as expected', async ()
       schedule: 'https://bnb.im/dist/schedule.json'
     }
     const staticData = await nodevu({ now, urls })
-    assert.deepStrictEqual(staticData.v17.releases['v17.0.0'].dependencies.v8, '9.5.172.21')
-    assert.deepStrictEqual(staticData.v14.support.lts.newest, '14.19.0')
-    assert.deepStrictEqual(staticData.v9.support.phases.current, 'end')
+
+    check(staticData)
   })
 
   it('should still return valid data when the index and the schedule are a different URL from the default while also using static now', async () => {
@@ -36,8 +49,7 @@ describe('check to make sure that combining options works as expected', async ()
       schedule: 'https://bnb.im/dist/schedule.json'
     }
     const staticData = await nodevu({ now, urls })
-    assert.deepStrictEqual(staticData.v17.releases['v17.0.0'].dependencies.v8, '9.5.172.21')
-    assert.deepStrictEqual(staticData.v14.support.lts.newest, '14.19.0')
-    assert.deepStrictEqual(staticData.v9.support.phases.current, 'end')
+
+    check(staticData)
   })
 })
---
+++ static/test/test.support.js
@@ -0,0 +1,30 @@
+const assert = require('node:assert')
+const staticData = require('../index')
+const { support } = require('../index')
+const { describe, it } = require('test')
+
+// always feel free to add more checks here - any addition will help ensure that we're even more solid.
+//
+// these tests are also generally copy/pasted between the test files - both in this file and in core. why?
+// specifically because we want to be sure the data is consistent *and* because we want to be sure that
+// the various ways of accessing the data produce consistent results.
+
+function check (data) {
+  // support checks
+  assert.deepStrictEqual(data.v14.codename, 'Fermium')
+  assert.deepStrictEqual(data.v12.lts.newest, '12.22.12')
+  assert.deepStrictEqual(data.v9.phases.current, 'end')
+  assert.deepStrictEqual(data.v8.phases.current, 'end')
+}
+
+describe('test known data in support.json export, being called from the default require', async () => {
+  it('should return the correct data for all types', () => {
+    check(staticData.support)
+  })
+})
+
+describe('test known data in support.json export, being called from a named require', async () => {
+  it('should return the correct data for all types', () => {
+    check(support)
+  })
+})
