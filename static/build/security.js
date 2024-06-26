91b9fbf8bdd986b874cb38eeec767304b40ed35f
Tierney Cyren <hello@bnb.im>
2024-06-26 16:08:01 -0400

    fix: remove async wrapper around static builders
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- static/build/security.js
+++ static/build/security.js
@@ -5,11 +5,10 @@ async function writeSecurity(filename) {
 	const data = await nodevu();
 	const security = {};
 
-	async () => {
-		for await (const version of Object.keys(data)) {
-			security[version] = data[version].security;
-		}
-	};
+	for await (const version of Object.keys(data)) {
+		security[version] = data[version].security;
+	}
+
 	write('./static/data/security.json', security);
 }
 
