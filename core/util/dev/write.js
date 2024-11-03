40c81c291db1eb10b1bd3c0a30b12041b1fb9ae8
Tierney Cyren <hello@bnb.im>
2024-11-03 12:42:29 +0000

    fix: use tabs rather than spaces
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- core/util/dev/write.js
+++ core/util/dev/write.js
@@ -2,7 +2,7 @@ const { writeFile } = require('node:fs/promises');
 const { resolve } = require('node:path');
 
 async function write(path, data) {
-	const outputJSON = JSON.stringify(data, null, 2);
+	const outputJSON = JSON.stringify(data, null, '\t');
 	const filepath = resolve(path);
 
 	await writeFile(filepath, outputJSON);
