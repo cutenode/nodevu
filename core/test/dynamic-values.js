d2e406530a9c5d44f2f9e92742f985e1a39ab2d8
Tierney Cyren <hello@bnb.im>
2024-11-03 19:48:37 +0700

    chore: use tabs rather than spaces to be consistent with linter (#61)
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- core/test/dynamic-values.js
+++ core/test/dynamic-values.js
@@ -21,12 +21,17 @@ describe('statically check that we get dynamic values correctly', async () => {
 		const staticData = await nodevu({ now });
 		assert.deepStrictEqual(staticData.v8.support.phases.current, 'end');
 		assert.deepStrictEqual(staticData.v14.support.phases.current, 'end');
+		assert.deepStrictEqual(staticData.v16.support.phases.current, 'end');
+		assert.deepStrictEqual(staticData.v17.support.phases.current, 'end');
 		assert.deepStrictEqual(
-			staticData.v16.support.phases.current,
+			staticData.v18.support.phases.current,
 			'maintenance',
 		);
-		assert.deepStrictEqual(staticData.v17.support.phases.current, 'end');
-		assert.deepStrictEqual(staticData.v18.support.phases.current, 'lts');
-		assert.deepStrictEqual(staticData.v20.support.phases.current, 'start');
+		assert.deepStrictEqual(
+			staticData.v20.support.phases.current,
+			'maintenance',
+		);
+		assert.deepStrictEqual(staticData.v22.support.phases.current, 'lts');
+		assert.deepStrictEqual(staticData.v23.support.phases.current, 'start');
 	});
 });
