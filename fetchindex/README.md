a63c5977f353c4fac18e2943538bdd2d66e3a0c2
Tierney Cyren <hello@bnb.im>
2024-11-08 21:08:43 +0000

    feat: add @nodevu/fetchindex
    Signed-off-by: Tierney Cyren <hello@bnb.im>

---
+++ fetchindex/README.md
@@ -0,0 +1,13 @@
+# @nodevu/fetchindex
+
+A tool that fetches the /dist/index.json file from the Node.js website.
+
+## Usage
+
+```js
+```
+
+```js
+```
+
+## API
---
+++ fetchindex/index.js
@@ -0,0 +1,13 @@
+async function versions(options) {
+	// parse our options and set up fetch if a custom fetch is passed
+	const fetch = options.fetch;
+	const url = options.urls.index;
+
+	// fetch the url, get the json from the fetched URL that we're going to use
+	const raw = await fetch(url);
+	const versions = await raw.json();
+
+	return versions;
+}
+
+module.exports = versions;
